10e97f92ee88482afdaf5fa37a1267b98b31e878
Gioia Frolik <gfrolik@student.tgm.ac.at>
2024-10-07 14:43:24 +0200

    new save and load classes

---
+++ src/main/java/org/gfrolik/SaveLoad.java
@@ -0,0 +1,12 @@
+package org.gfrolik;
+
+/**
+ * Flashcard deck
+ * @author gioia
+ * @version 2024-09-30
+ */
+public interface SaveLoad {
+    public void save();
+
+    public void load();
+}
---
+++ src/main/java/org/gfrolik/SaveLoadJSON.java
@@ -0,0 +1,18 @@
+package org.gfrolik;
+
+/**
+ * Flashcard deck
+ * @author gioia
+ * @version 2024-09-30
+ */
+public class SaveLoadJSON implements SaveLoad {
+    @Override
+    public void save() {
+
+    }
+
+    @Override
+    public void load() {
+
+    }
+}
