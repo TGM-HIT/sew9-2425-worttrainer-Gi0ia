d17b5935336455d2009cd922727c85b1abfa3354
Gioia Frolik <gfrolik@student.tgm.ac.at>
2024-10-11 08:55:53 +0200

    parameter typ of URL from String to URL

--- src/main/java/org/gfrolik/Flashcard.java
+++ src/main/java/org/gfrolik/Flashcard.java
@@ -1,6 +1,10 @@
 package org.gfrolik;
 import java.net.URL;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * MODEL LAYER
@@ -9,44 +13,58 @@ import java.net.MalformedURLException;
  * @version 2024-09-30
  */
 public class Flashcard {
-    private String frontURL;
-    private String backAnswer;
+    private static final Logger LOGGER = Logger.getLogger(Flashcard.class.getName()); // Logger instance
+
+    private URL URL;
+    private String word;
 
     // constructor
-    public Flashcard(String frontURL, String backAnswer) {
-        if(isValid()){
-            this.frontURL = frontURL;
-            this.backAnswer = backAnswer;
-        }
-        else {
-            this.frontURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Brigittenau_%28Wien%29_-_TGM-Hauptgeb%C3%A4ude.JPG/280px-Brigittenau_%28Wien%29_-_TGM-Hauptgeb%C3%A4ude.JPG";
-            this.backAnswer = "tgm";
+    public Flashcard(URL frontURL, String backWord) {
+        if (isValid()) {
+            this.URL = frontURL;
+            this.word = backWord;
+        } else {
+            try {
+                URI uri = new URI("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Brigittenau_%28Wien%29_-_TGM-Hauptgeb%C3%A4ude.JPG/280px-Brigittenau_%28Wien%29_-_TGM-Hauptgeb%C3%A4ude.JPG");
+                this.URL = uri.toURL();  // Convert URI to URL
+            } catch (URISyntaxException | MalformedURLException e) {
+                LOGGER.log(Level.SEVERE, "Error while creating the fallback URL", e);  // Log the error
+            }
+            this.word = "tgm";
         }
     }
+
 
     // check if the values are valid
     public boolean isValid() {
-        if(this.backAnswer==null)
+        // Check if 'word' is null
+        if (this.word == null || this.word.isEmpty()) {
             return false;
-        if(this.frontURL!=null){
+        }
+
+        // Check if 'URL' is not null and is valid
+        if (this.URL != null) {
             try {
-                new URL(this.frontURL);         // Try to create a URL object
-            } catch (MalformedURLException e) {
-                return false;                   // If a MalformedURLException is thrown, the URL is invalid
+                // Convert the URL to a URI to check if it's valid
+                this.URL.toURI();  // URL has a toURI() method, which throws URISyntaxException if invalid
+            } catch (URISyntaxException e) {
+                return false;  // Return false if the URL is invalid
             }
         }
-        return true;                            // If no exception is thrown, the URL is valid
+
+        return true;  // If no issues, return true
     }
 
+
     // return the URL (front)
-    public String getFrontURL() {
-        return frontURL;
+    public java.net.URL getURL() {
+        return URL;
     }
 
 
     // return the correct answer (back)
-    public String getBackAnswer() {
-        return backAnswer;
+    public String getWord() {
+        return word;
     }
 
 }
--- src/main/java/org/gfrolik/FlashcardDeck.java
+++ src/main/java/org/gfrolik/FlashcardDeck.java
@@ -1,8 +1,9 @@
 package org.gfrolik;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.io.IOException;
+import java.net.*;
+import java.util.*;
+import java.util.logging.*;
 
 
 /**
@@ -14,9 +15,10 @@ import java.util.Random;
 public class FlashcardDeck {
     private String deckName;
     private String language;
+    private static final Logger LOGGER = Logger.getLogger(Flashcard.class.getName());
 
-    private List<Flashcard> cards;
     private Flashcard currentCard;
+    private List<Flashcard> cards;
     private Statistics stats;
 
 
@@ -30,9 +32,13 @@ public class FlashcardDeck {
 
     public FlashcardDeck() {
         List<Flashcard> flashcards = new ArrayList<>();
-        flashcards.add(new Flashcard("https://i.pinimg.com/originals/a4/4b/af/a44baf314bd7fda48430259e7365bf7f.jpg", "spaghetti"));
-        flashcards.add(new Flashcard("https://www.radsport-wagner.at/wp-content/uploads/2023/06/516caf81670d0566f088d5f4fed58b30-e1704898281552.png", "bicicletta"));
-        flashcards.add(new Flashcard("https://perfectdailygrind.com/es/wp-content/uploads/sites/2/2019/11/espresso.jpg", "espresso"));
+        try {
+            flashcards.add(new Flashcard(new URL("https://i.pinimg.com/originals/a4/4b/af/a44baf314bd7fda48430259e7365bf7f.jpg"), "spaghetti"));
+            flashcards.add(new Flashcard(new URL("https://www.radsport-wagner.at/wp-content/uploads/2023/06/516caf81670d0566f088d5f4fed58b30-e1704898281552.png"), "bicicletta"));
+            flashcards.add(new Flashcard(new URL("https://perfectdailygrind.com/es/wp-content/uploads/sites/2/2019/11/espresso.jpg"), "espresso"));
+        } catch (MalformedURLException e) {
+            LOGGER.log(Level.SEVERE, "Error while creating a URL for a flashcard", e);
+        }
 
         this.deckName = "ITALIANO DECK 1";
         this.language = "italian";
@@ -48,15 +54,20 @@ public class FlashcardDeck {
     // select random card from deck
     public Flashcard selectRandomCard() {
         Random random = new Random();
-        return cards.get(random.nextInt(cards.size()));
+        Flashcard f = cards.get(random.nextInt(cards.size()));
+        setCurrentCard(f);
+        return f;
+    }
 
+    public List<Flashcard> getFlashcards() {
+        return cards;
     }
 
 
     // returns true if user input is correct & increments the count
     public boolean checkUserInput(String input) {
 
-        if (this.currentCard.getBackAnswer().equalsIgnoreCase(input)) {
+        if (this.currentCard.getWord().equalsIgnoreCase(input)) {
             stats.incrementCorrectAttempts();
             return true;
         }
@@ -67,23 +78,47 @@ public class FlashcardDeck {
 
     }
 
-    // returns stats as String
-    public String getStatistics() {
-        return stats.getStatistics();
+    // Getter and setter for statistics
+    public Statistics getStatistics() {
+        return stats;
     }
 
+    public Flashcard getCurrentCard() {
+        return currentCard;
+    }
+
+    public void setStatistics(Statistics statistics) {
+        this.stats = statistics;
+    }
     // resets the stats
     public void resetTrainerStatistics() {
         stats.resetStatistics();
     }
 
-    public void saveSession() {
-
+    // Save session method
+    public void saveSession(SaveLoad saver, String filePath) {
+        try {
+            saver.save(this, filePath);
+            System.out.println("Session saved to " + filePath);
+        } catch (IOException e) {
+            System.err.println("Error saving session: " + e.getMessage());
+        }
     }
 
-    public void loadSession() {
-
+    // Load session method
+    public void loadSession(SaveLoad loader, String filePath) {
+        try {
+            FlashcardDeck loadDeck = loader.load(filePath);
+            this.cards = loadDeck.getFlashcards();
+            this.stats = loadDeck.getStatistics();
+            System.out.println("Session loaded from " + filePath);
+        } catch (IOException e) {
+            System.err.println("Error loading session: " + e.getMessage());
+        }
     }
 
 
+    public void addFlashcard(Flashcard f) {
+        this.cards.add(f);
+    }
 }
